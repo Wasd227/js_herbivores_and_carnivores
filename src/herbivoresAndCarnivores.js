87a7183a8a1fd3fbc1ddca0af434c2e14640dd6b
Misha shimchishin git config --global user,email mykhailo.shimchishin@icloud,com <you@example.com>
2025-02-28 00:55:19 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+    this.hidden = false;
+    this.type = 'Animal';
+    Animal.alive.push(this);
+  }
+
+  checkHealth() {
+    if (this.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== this);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.type = 'Herbivore';
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.type = 'Carnivore';
+  }
+
+  bite(prey) {
+    if (prey.type === 'Herbivore' && !prey.hidden) {
+      prey.health -= 50;
+      prey.checkHealth();
+    }
+  }
 }
 
 module.exports = {
